832b56af56519d31b1c216cce7d6f068c2af63a1
Giant77 <willyarsyad77@gmail.com>
2025-11-20 09:16:25 +0700

    chore: remove unused app

--- RentalMotor/payments/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class AddOn(models.Model):
-    code = models.CharField(max_length=50, unique=True)
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
